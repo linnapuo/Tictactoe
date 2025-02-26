edaf08ab0436ce7695272d8c09c5e2399008cc7a
Teemu Linnapuomi <teemu.linnapuomi@vincit.fi>
2025-02-26 06:53:52 +0200

    Configure cors AllowedOrigins in AppHost

--- Tictactoe.AppHost/Program.cs
+++ Tictactoe.AppHost/Program.cs
@@ -2,13 +2,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var server = builder.AddProject<Projects.Tictactoe_Server>("tictactoe-server");
 
-builder.AddNpmApp("tictactoe-client", "../tictactoe-client", "dev")
+var front = builder.AddNpmApp("tictactoe-client", "../tictactoe-client", "dev")
     .WithReference(server)
+    .WaitFor(server)
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
-builder.AddProject<Projects.Tictactoe_Authentication>("tictactoe-authentication");
+server.WithEnvironment(ctx =>
+{
+    ctx.EnvironmentVariables["AllowedOrigins"] = front.Resource.GetEndpoint("http");
+});
 
 builder.Build().Run();
